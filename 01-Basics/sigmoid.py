cbd18a196a1c8444523233aa5777e05e56082fc0
David Torres <31328174+datorresb@users.noreply.github.com>
2017-11-07 12:17:46 -0500

    Add files via upload

---
+++ 01-Basics/sigmoid.py
@@ -0,0 +1,17 @@
+# GRADED FUNCTION: sigmoid
+def sigmoid(x):
+    """
+    Compute the sigmoid of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size
+
+    Return:
+    s -- sigmoid(x)
+    """
+    
+    ### START CODE HERE ### (≈ 1 line of code)
+    s = 1/(1+np.exp(-x))
+    ### END CODE HERE ###
+    
+    return s
